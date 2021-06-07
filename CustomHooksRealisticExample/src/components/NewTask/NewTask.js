673ded207347bbde9418703a716f900a55ff3c7b
Mubeen6036 <s.mubeenmusthafa@gmail.com>
2021-06-07 21:40:32 +0530

    final changes for Custom hook

--- CustomHooksRealisticExample/src/components/NewTask/NewTask.js
+++ CustomHooksRealisticExample/src/components/NewTask/NewTask.js
@@ -1,41 +1,29 @@
-import { useState } from 'react';
+import useHTTP from '../../hooks/useHTTP';
 
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+
+  const transformTasks = (data, taskText) =>{
+    const generatedId = data.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+    console.log(createdTask);
+    props.onAddTask(createdTask);
+  }
+
+  const {isLoading, error, sendRequest : setTasks} = useHTTP(transformTasks);
+
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://dark-man-416e9-default-rtdb.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
+    setTasks({
+      url :  'https://dark-man-416e9-default-rtdb.firebaseio.com/tasks.json',
+      method: 'POST',
+      body: { text: taskText },
+      headers: {
+        'Content-Type': 'application/json',
       }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
+    });
   };
 
   return (
